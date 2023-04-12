6b70113f349ee7985f042f2ffa14230088dc66f6
YasheeSachan <71555619+YasheeSachan@users.noreply.github.com>
2023-04-12 10:09:25 +0530

    Update LoginController.java

--- src/main/java/com/FYP/shramik1/Controller/LoginController.java
+++ src/main/java/com/FYP/shramik1/Controller/LoginController.java
@@ -4,6 +4,7 @@ package com.FYP.shramik1.Controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
